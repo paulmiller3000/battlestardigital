c751a90f9d4276c2d772a7aa0c5b9e2ff3790b4e
Paul Miller <paul@battlestardigital.com>
2019-02-23 15:37:20 -0500

    Minimized images; added security, compression, expires to htaccess, further minified JS, removed Font Awesome

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -29,12 +29,12 @@
 
     <!-- Optional JavaScript -->
     <!-- Bootstrap -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	
 	<!-- Custom -->
-	<script src="<?php echo URLROOT; ?>/js/jquery.waypoints.min.js" type="text/javascript"></script>	
-	<script src="<?php echo URLROOT; ?>/js/scripts.min.js" type="text/javascript"></script>
+	<script defer src="<?php echo URLROOT; ?>/js/jquery.waypoints.min.js" type="text/javascript"></script>	
+	<script defer src="<?php echo URLROOT; ?>/js/scripts.min.js" type="text/javascript"></script>
 </body>
 </html>
